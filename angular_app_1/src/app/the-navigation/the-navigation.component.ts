43451b38bfd7f7b6813df72f2781c200f023cce2
Michelet Saint-Preux <michelet2000@gmail.com>
2022-12-09 15:58:25 +0800

    angular_app_1/src/app/the-navigation/the-navigation.component.css angular_app_1/src/app/the-navigation/the-navigation.component.html angular_app_1/src/app/the-navigation/the-navigation.component.ts angular_app_1/src/app/the-section/the-section.component.css

--- angular_app_1/src/app/the-navigation/the-navigation.component.ts
+++ angular_app_1/src/app/the-navigation/the-navigation.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, HostBinding, Inject, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-the-navigation',
   templateUrl: './the-navigation.component.html',
   styleUrls: ['./the-navigation.component.css']
 })
-export class TheNavigationComponent {
+export class TheNavigationComponent{
+  /*
+  isFixedNavbar: boolean =false;
+  @HostBinding('class.navbar-opened') navbarOpened = false;
+  constructor(
+  ) { }
+  ngOnInit() {
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const offset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if(offset > 10) {
+      this.isFixedNavbar = true;
+    } else {
+      this.isFixedNavbar = false;
+    }
+  }
+
+  toggleNavbar() {
+    this.navbarOpened = !this.navbarOpened;
+  }*/
   /*
   showLinked: boolean = false;
 
