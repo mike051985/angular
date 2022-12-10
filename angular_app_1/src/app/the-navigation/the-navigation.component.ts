f4f6edcc0cf723d3741d990358816b8b9a00794c
Michelet Saint-Preux <michelet2000@gmail.com>
2022-12-10 08:48:06 +0800

    angular_app_1/package-lock.json angular_app_1/package.json angular_app_1/src/app/the-navigation/the-navigation.component.css angular_app_1/src/app/the-navigation/the-navigation.component.html angular_app_1/src/app/the-navigation/the-navigation.component.ts angular_app_1/src/app/the-section/the-section.component.css angular_app_1/src/app/the-section/the-section.component.html

--- angular_app_1/src/app/the-navigation/the-navigation.component.ts
+++ angular_app_1/src/app/the-navigation/the-navigation.component.ts
@@ -1,4 +1,6 @@
-import { Component,OnInit, HostBinding, Inject, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-the-navigation',
@@ -6,35 +8,5 @@ import { Component,OnInit, HostBinding, Inject, HostListener } from '@angular/co
   styleUrls: ['./the-navigation.component.css']
 })
 export class TheNavigationComponent{
-  /*
-  isFixedNavbar: boolean =false;
-  @HostBinding('class.navbar-opened') navbarOpened = false;
-  constructor(
-  ) { }
-  ngOnInit() {
-  }
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const offset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    if(offset > 10) {
-      this.isFixedNavbar = true;
-    } else {
-      this.isFixedNavbar = false;
-    }
-  }
-
-  toggleNavbar() {
-    this.navbarOpened = !this.navbarOpened;
-  }*/
-  /*
-  showLinked: boolean = false;
-
-  showLinks() {
-    return this.showLinked;
-  }
-
-  click() {
-    this.showLinked = !this.showLinked
-  }*/
+  
 }
